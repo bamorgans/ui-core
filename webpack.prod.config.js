945fe43abb1e7dc03a1c8888224eb7bda9b325cc
Bob Morganstern <bmorganstern@paloaltonetworks.com>
2018-04-12 12:01:49 -0700

    - Configured Webpack to create Development and Production Libraries - Add Readme describing how to use this module, create Library Elements and Components, and setup Live Editing for development - added sample html page to using the ui-core as a standalone library

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,21 +1,54 @@
-const config = require('./webpack.config.js');
+const path = require('path');
 const webpack = require('webpack');
+const merge = require ('webpack-merge');
 
-config.plugins.push(
 
-    new webpack.DefinePlugin({
-        'process.env': {
-            NODE_ENV: JSON.stringify('production')
-        }
-    })
-);
+const common = require('./webpack.common.config.js');
 
-config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false
-        }
-    })
-);
 
-module.exports = config;
+const APP_SRC = path.resolve(__dirname, 'src');
+const APP_LIB = path.resolve(__dirname, 'lib');
+
+module.exports = merge (common, {
+    output: {
+        path: APP_LIB,
+        filename: 'ui-core.min.js',
+        library: 'uiCore',
+        libraryTarget: 'umd'
+    },
+    devtool: false,
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+
+        new webpack.optimize.UglifyJsPlugin({
+            uglifyOptions: {
+                ecma: 6,
+                safari10: true,
+                compress: {
+                    warnings: false,
+                    screw_ie8: true,
+                    conditionals: true,
+                    unused: true,
+                    comparisons: true,
+                    sequences: true,
+                    dead_code: true,
+                    evaluate: true,
+                    if_return: true,
+                    join_vars: true
+                },
+                mangle: true
+
+            },
+            output: {
+                comments: false
+            },
+            sourceMap: false,
+            exclude: [/\.min\.js$/gi] // skip pre-minified libs
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin()
+    ]
+});
