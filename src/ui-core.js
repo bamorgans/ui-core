b76885aedcb8708f079e41f3447fded43d39b2c7
Bob Morganstern <bmorganstern@paloaltonetworks.com>
2018-04-13 16:36:44 -0700

    Fixed code so that the library UI can be used as standalone React Components - Update the sample html for Standalone React - updated the README

--- src/ui-core.js
+++ src/ui-core.js
@@ -1,3 +1,5 @@
+import './assets/css/core-ui.css';
+
 export { default as Button } from './elements/button/Button';
 export { default as Button2 } from './elements/button/Button2';
 export { default as ButtonSVG } from './elements/button/ButtonSVG';
